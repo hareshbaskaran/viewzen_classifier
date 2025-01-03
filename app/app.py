3d3eb31630af594e3d331a300c23194bf135dc5e
haresh <baskaranharesh@gmail.com>
2025-01-03 13:46:57 +0530

    fix : webapp

--- app/app.py
+++ app/app.py
@@ -2,10 +2,8 @@ import streamlit as st
 import requests
 from utils.configs import API_ENDPOINT_URL
 
-
 st.title("Iris Flower Prediction")
 st.write("Please provide the following details about the Iris flower:")
-
 
 sepal_length = st.number_input(
     "Sepal Length (cm)", min_value=0.0, max_value=10.0, step=0.1
@@ -20,7 +18,6 @@ petal_width = st.number_input(
     "Petal Width (cm)", min_value=0.0, max_value=10.0, step=0.1
 )
 
-
 if st.button("Predict"):
     data = {
         "sepal_length": sepal_length,
@@ -29,16 +26,13 @@ if st.button("Predict"):
         "petal_width": petal_width,
     }
 
-    response = requests.post(API_ENDPOINT_URL, data=data)
+    # Send JSON data in the request
+    response = requests.post(API_ENDPOINT_URL, json=data)
 
-    ## check response code
 
     if response.status_code == 200:
         prediction = response.json()
         st.write(f"Predicted Class: {prediction['predicted_class']}")
         st.write(f"Predicted Class Name: {prediction['predicted_class_name']}")
     else:
-        st.write("Error in prediction. Please try again.")
-
-# uvicorn app.main:app --reload
-# streamlit run app/app.py
+        st.write(f"Error in prediction. Status code: {response.status_code}")
