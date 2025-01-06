950fc0d508451967cc1a8bbbe1b152d73bc01ec0
haresh <baskaranharesh@gmail.com>
2025-01-06 17:09:22 +0530

    fix : web, db services

--- app/utils/configs.py
+++ app/utils/configs.py
@@ -1,3 +1,3 @@
 MODEL_PATH = "app/data/iris_model.pkl"
-API_ENDPOINT_URL = "http://localhost:80/predict"
+API_ENDPOINT_URL = "http://localhost:8080/predict"
 #API_ENDPOINT_URL = "http://127.0.0.1:8000/predict/" (local)
